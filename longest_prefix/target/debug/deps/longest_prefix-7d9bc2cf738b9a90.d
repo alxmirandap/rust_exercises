1c25922762cc3933fea8d80522600ae8ee60ebbe
alexpArtos <39948493+alexpArtos@users.noreply.github.com>
2024-08-02 10:44:55 +0100

    Delete longest_prefix/target directory

--- longest_prefix/target/debug/deps/longest_prefix-7d9bc2cf738b9a90.d
+++
@@ -1,5 +0,0 @@
-/home/alexandre/Documents/Code_Exercises/rust/longest_prefix/target/debug/deps/longest_prefix-7d9bc2cf738b9a90: src/main.rs
-
-/home/alexandre/Documents/Code_Exercises/rust/longest_prefix/target/debug/deps/longest_prefix-7d9bc2cf738b9a90.d: src/main.rs
-
-src/main.rs:
--- longest_prefix/target/debug/longest_prefix.d
+++
@@ -1,1 +0,0 @@
-/home/alexandre/Documents/Code_Exercises/rust/longest_prefix/target/debug/longest_prefix: /home/alexandre/Documents/Code_Exercises/rust/longest_prefix/src/main.rs
